6cac67e84f5350f6872cf9ba5c2bf844eed8fd91
adriafretgarcia <adriafretgarcia@S1-46>
2017-02-09 11:32:42 +0100

    Ahgora este programa te printa hello Github

--- HelloGitHub/HelloGitHub.cpp
+++ HelloGitHub/HelloGitHub.cpp
@@ -1,6 +1,3 @@
-// HelloGitHub.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 
 
---
+++ HelloGitHub/Source.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <iostream>
+
+
+
+
+int main(){
+	
+	using namespace std;
+
+	cout << "Hello GitHub";
+	
+	return 0;
+}
