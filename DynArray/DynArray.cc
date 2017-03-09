17d2dd7e9a17889f047c7554a6caad428789d3d0
adriafretgarcia <adriafretgarcia@S1-46>
2017-03-09 11:29:40 +0100

    Push, Pop, Erase

--- DynArray/DynArray.cc
+++ DynArray/DynArray.cc
@@ -1,38 +1,41 @@
 #include "DynArray.hh"
 
-DynArray::DynArray(void) {
-	m_capacity = DYN_ARRAY_DEFAULT_SIZE;
-	m_size = 0;
-	m_data = new int[m_capacity];
-
+DynArray::DynArray(void) :
+	m_capacity(DYN_ARRAY_DEFAULT_SIZE),
+	m_size(0),
+	m_data(new int[m_capacity])
+{
 }
 
-DynArray::DynArray(size_t size)
+DynArray::DynArray(size_t size):
+
+	m_capacity(size),
+	m_size(size),
+	m_data(new int[m_capacity])
+
 {
-	m_capacity = size;
-	m_size = size;
-	m_data = new int[m_capacity];
 }
 
-DynArray::DynArray(size_t size, const int & value)
+DynArray::DynArray(size_t size, const int & value):
+
+	m_capacity(size),
+	m_size(size),
+	m_data(new int[m_capacity])
+	
 {
-	m_capacity = size;
-	m_size = size;
-	m_data = new int[m_capacity];
-	for (int i = 0; i < static_cast<int>(size);i++) {
-		m_data[i] = value;
-	}
+		/*for (int i = 0; i < static_cast<int>(size);i++) {
+			m_data[i] = value;
+		}*/
+	fill(begin(), end(), value);
 }
 
-DynArray::DynArray(int * arr, size_t size)
+DynArray::DynArray(int * arr, size_t size):
+
+	m_capacity(size),
+	m_size(size),
+	m_data(new int[m_capacity])
 {
-	m_capacity = size;
-	m_size = size;
-	m_data = new int[m_capacity];
-
-	//Recorrerla con punteros sería mejor?
-
-	for (int i = 0; i < sizeof(arr)/4; i++)
+	for (int i = 0; i < static_cast<int>(m_size); i++)
 	{
 		m_data[i] = arr[i];
 	}
@@ -52,7 +55,7 @@ int * DynArray::begin(void) const
 int * DynArray::end(void) const
 {
 
-	return (m_data+m_capacity);
+	return (m_data+m_size);
 }
 
 
@@ -125,18 +128,100 @@ int & DynArray::back(void) const
 	return m_data[m_size - 1];
 }
 
+int * DynArray::data(void) const
+{
+	return m_data;
+}
+
+void DynArray::push(const int & val)
+{
+	if (m_size < m_capacity) {
+		m_data[m_size] = val;
+		m_size++;
+	}
+	else {
+		int *tmp = new int[m_capacity + 1];
+		for (int i = 0; i < m_size; i++)
+		{
+			tmp[i] = m_data[i];
+		}
+		tmp[m_size] = val;
+		m_size = m_size + 1;
+		m_capacity = m_capacity + 1;
+		delete[] m_data;
+		m_data = tmp;
+	}
+}
+
+void DynArray::pop(void)
+{
+		int *tmp = new int[m_capacity];
+		for (int i = 0; i < m_size-1; i++)
+		{
+			tmp[i] = m_data[i];
+		}
+		m_size = m_size - 1;
+		m_capacity = m_capacity;
+		delete[] m_data;
+		m_data = tmp;
+}
+
+void DynArray::erase(size_t position)
+{
+	int *tmp = new int[m_capacity];
+	/*for (int i = 0; i < m_size; i++)
+	{
+		if (i != position) {
+			tmp[i] = m_data[i];
+		}	
+	}*/
+	int i = 0, y = 0;
+	while (i < m_size) {
+		if (y == position) {
+			y++;
+		}
+		else {
+			tmp[i] = m_data[y];
+			i++;
+			y++;
+		}
+	}
+	m_size = m_size-1;
+	m_capacity = m_capacity;
+	delete[] m_data;
+	m_data = tmp;
+}
+
 bool operator==(const DynArray & lhs, const DynArray & rhs)
 {
 	if (lhs.m_size == rhs.m_size) {
-		//Tienen longitud igual
 		int cont = 0;
+		
+		while (cont < static_cast<int>(lhs.m_size))
+		{
+			if (lhs[cont] != rhs[cont]) {
+				return false;
+			}
+			cont++;
+		}
+	}
+		return true;
+}
+
+bool operator!=(const DynArray & lhs, const DynArray & rhs)
+{
+	if (lhs.m_size == rhs.m_size) {
+		int cont = 0;
+
 		while (cont < static_cast<int>(lhs.m_size))
 		{
 			if (lhs[cont] == rhs[cont]) {
-				return true;
+				return false;
 			}
 		}
 	}
+		return true;
 
-	return false;
+	//return !(lhs==rhs);
 }
+
--- DynArray/main.cc
+++ DynArray/main.cc
@@ -4,23 +4,18 @@
 
 
 int main() {
-	DynArray a1;
-	DynArray a2(35, 14);
-	
-	int *p = a1.begin();
-	int *t = a1.end();
+	DynArray a2(5, 8);
+	a2.push(5);
+	a2.push(5);
+	a2.push(5);
+	a2.push(200);
+
+	//a2.pop();
+	a2.erase(7);
 
 	int *p2 = a2.begin();
 	int *t2 = a2.end();
-	std::cout << p << " / " << t;
-	std::cout << "\n";
 	std::cout << p2 << " / " << t2;
-	std::cout << "Contructor 1: \n";
-	while (p != t)
-	{
-		std::cout << *p << "\t";
-		p++;
-	}
 
 	std::cout << "\nContructor 3: \n";
 	while (p2 != t2)
@@ -29,8 +24,8 @@ int main() {
 		p2++;
 	}
 
-	std::cout << "\nRelleno el 1 con 9: \n";
+	std::cout << a2.size() << "\n";
 
-	std::cout << a2.size();
+
 	return 0;
 }
